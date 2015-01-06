9af92a85acb4b5b99d2fd79212b1c5fb7a05c0d5
Vladimir Okhotnikov <vokhotnikov@gmail.com>
2015-01-06 15:47:11 +0300

    Basic data load/subset/convert

--- plot1.R
+++ plot1.R
@@ -10,5 +10,10 @@ readData <- function() {
                     comment.char = "",
                     colClasses = c(rep("character", 2), rep("numeric", 7)))
     
-    d
+    
+    d1 <- d[grepl("^[12]/2/2007$", d$Date),]
+    
+    d1$DateTime <- strptime(paste(d1$Date, d1$Time), "%e/%m/%Y%t%H:%M:%S")
+    
+    d1
 }
